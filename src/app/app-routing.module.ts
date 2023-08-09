ea87ff63da09ac3e7d00a6911b54e3e29defe24b
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-08-09 12:52:18 +0300

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path:"signin", component: SignInComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
