8bb2b7644c9bfd7385bb87a4ffa92dccd231468a
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-08-21 22:17:36 +0300

    created filter component

---
+++ src/app/components/filter/filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss']
+})
+export class FilterComponent {
+  priceFilterToggle: boolean = false;
+  categoryFilterToggle: boolean = false;
+}
