ecc6c1896aed17c43a6be605b0d11f7046b227bd
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-08-21 22:15:49 +0300

    added store and cart routing, used hash in url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 const routes: Routes = [
   {path:"home", loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path:"store", loadChildren: () => import('./pages/store/store.module').then(m => m.StoreModule)},
+  {path:"cart", component: CartComponent},
   {path:"signin", component: SignInComponent},
   {path: '', redirectTo:"home", pathMatch: 'full'},
   {path: "**", loadChildren: () => import('./pages/error/error.module').then(m => m.ErrorModule)},
@@ -11,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
