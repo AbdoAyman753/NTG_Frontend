96bf14efc50d706dcff42b6769fc711387719dda
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-08-21 22:16:46 +0300

    added some modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,24 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeModule } from './pages/home/home.module';
 import { ErrorComponent } from './pages/error/error.component';
 import { ErrorModule } from './pages/error/error.module';
+import { StoreComponent } from './pages/store/store.component';
+import { StoreModule } from './pages/store/store.module';
+import { ItemCardComponent } from './components/item-card/item-card.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     HomeModule,
+    StoreModule,
     ErrorModule,
     AppRoutingModule
   ],
