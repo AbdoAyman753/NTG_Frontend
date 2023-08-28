79c0a9f822adf54a4f7d2bcc313ceea73a2407f7
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-08-28 14:43:18 +0300

    created game service to fetch data from backend

---
+++ src/app/services/games.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { Game } from '../entities/Game';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GamesService {
+  // Define API
+  apiURL = 'http://localhost:8080/api/v1/games';
+  constructor(private http: HttpClient) { }
+
+  // Http Options
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  /*========================================
+    CRUD Methods for consuming RESTful API
+  =========================================*/
+
+  getGames(): Observable<Game> {
+    return this.http
+      .get<Game>(this.apiURL)
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+  // Error handling
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(() => {
+      return errorMessage;
+    });
+  }
+}
