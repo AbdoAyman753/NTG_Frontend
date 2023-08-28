bed6e229cdb846890022b261f6071f0b5df169de
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-08-28 14:46:33 +0300

    fetched data using service

--- src/app/pages/store/store.component.ts
+++ src/app/pages/store/store.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GamesService } from 'src/app/services/games.service';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.scss']
+  styleUrls: ['./store.component.scss'],
+  providers: [GamesService]
 })
-export class StoreComponent {
-  Heros = [{}, {}, {}, {}, {}, {}];
+export class StoreComponent implements OnInit {
+  games:any = [];
+  constructor(public gamesService: GamesService){
+
+  }
+  ngOnInit() {
+    // this.gamesService.getGames().subscribe((data:any)=>{
+    // this.games = data;});
+  }
 }
