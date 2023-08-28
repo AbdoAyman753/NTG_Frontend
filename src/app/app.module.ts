b2a592d6b3ccc5cd51fde0124acc3ed1d4608f8f
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-08-28 14:40:28 +0300

    added gameService to fetch data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeModule } from './pages/home/home.module';
-import { ErrorComponent } from './pages/error/error.component';
 import { ErrorModule } from './pages/error/error.module';
-import { StoreComponent } from './pages/store/store.component';
 import { StoreModule } from './pages/store/store.module';
-import { ItemCardComponent } from './components/item-card/item-card.component';
-import { FilterComponent } from './components/filter/filter.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { GamesService } from './services/games.service';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
@@ -29,9 +27,10 @@ import { CartComponent } from './pages/cart/cart.component';
     HomeModule,
     StoreModule,
     ErrorModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [GamesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
