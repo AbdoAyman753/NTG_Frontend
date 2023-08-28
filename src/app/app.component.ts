5738b30af41d2494db9306c6b2975a417cca65d8
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-08-28 14:39:33 +0300

    used gameService to get data from backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { GamesService } from './services/games.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers:[GamesService]
 })
 export class AppComponent {
   title = 'NTG_Frontend';
