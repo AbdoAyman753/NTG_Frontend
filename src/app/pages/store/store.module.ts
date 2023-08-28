f0c5a7bd47bc8c320839bb98942a2e8cabf748e4
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-08-28 14:46:53 +0300

    added spinner component and gameService

--- src/app/pages/store/store.module.ts
+++ src/app/pages/store/store.module.ts
@@ -5,18 +5,22 @@ import { StoreRoutingModule } from './store-routing.module';
 import { StoreComponent } from './store.component';
 import { ItemCardComponent } from 'src/app/components/item-card/item-card.component';
 import { FilterComponent } from 'src/app/components/filter/filter.component';
+import { GamesService } from 'src/app/services/games.service';
+import { SpinnerComponent } from 'src/app/components/spinner/spinner.component';
 
 
 @NgModule({
   declarations: [
     StoreComponent,
     FilterComponent,
-    ItemCardComponent
+    ItemCardComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule,
     StoreRoutingModule
   ],
-  exports:[StoreComponent]
+  exports:[StoreComponent],
+  providers:[GamesService]
 })
 export class StoreModule { }
