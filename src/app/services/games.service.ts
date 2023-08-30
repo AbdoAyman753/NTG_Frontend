66fee36487e1c736a84bb9a573f1cf38f770cbc8
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-08-30 13:58:03 +0300

    added filteration to games

--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 import { Game } from '../entities/Game';
+import { Category } from '../entities/Category';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { Game } from '../entities/Game';
 export class GamesService {
   // Define API
   apiURL = 'http://localhost:8080/api/v1/games';
+  categoriesURL = 'http://localhost:8080/api/v1/categories';
   constructor(private http: HttpClient) { }
 
   // Http Options
@@ -24,11 +26,39 @@ export class GamesService {
   =========================================*/
 
   getGames(): Observable<Game> {
+    let url = this.apiURL +"?minPrice=0&maxPrice=10000";
     return this.http
-      .get<Game>(this.apiURL)
+      .get<Game>(url)
       .pipe(retry(1), catchError(this.handleError));
   }
-
+  // getGamesFiltered(minPrice: number, maxPrice: number,categories:[]): Observable<Game>{
+  //   return this.http
+  //               .get<Game>(this.apiURL+"?minPrice="+minPrice
+  //                         +"&maxPrice="+maxPrice
+  //                         +"&categories="+categories)
+  //               .pipe(retry(1), catchError(this.handleError));
+  // }
+  getGamesFiltered(minPrice: number, maxPrice: number,categories:[]){
+    let url = this.apiURL+"?minPrice="+minPrice+"&maxPrice="+maxPrice;
+    if(categories.length != 0){
+      url += "&categories=";
+      categories.forEach((category:any, index:number) => {
+        url += category?.id;
+        if(index != categories.length -1){
+          url += ","
+        }
+      });
+    }
+    // console.log(url);
+    return this.http
+      .get<Game>(url)
+      .pipe(retry(1), catchError(this.handleError));
+  }
+  getCategories():Observable<Category>{
+    return this.http
+          .get<Category>(this.categoriesURL)
+          .pipe(retry(1), catchError(this.handleError));
+  }
   // Error handling
   handleError(error: any) {
     let errorMessage = '';
