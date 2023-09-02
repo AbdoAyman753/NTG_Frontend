c7e03d1657bbf1fce5900e66cb878970bdd765ac
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-09-02 16:32:44 +0300

    created user class

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+export class User{
+  id:any;
+  firstName:any;
+  lastName:any;
+  userName:any;
+  email:any;
+  password:any;
+  profilePicture:any;
+  role:any;
+  cartGames:any;
+  library:any;
+}
