aca5099d07069ad6c533e8c44586de50dd82f64e
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-09-02 16:39:51 +0300

    add retrieving data from cookies at start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GamesService } from './services/games.service';
+import { TokenService } from './services/token.service';
+import { UserService } from './services/user.service';
+import { UserApiService } from './services/user-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers:[GamesService]
+  providers: [GamesService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy {
   title = 'NTG_Frontend';
+
+  constructor(
+    private tokenService: TokenService,
+    private userService: UserService,
+  ) {}
+  ngOnInit(): void {
+    this.userService.updateUser(this.tokenService.getUser());
+    // console.log(this.tokenService.getUser());
+    // window.addEventListener('beforeunload', this.updateCart);
+
+  }
+
+  ngOnDestroy(): void {
+    // window.removeEventListener('beforeunload',this.updateCart);
+  }
 }
