9b014fd4253be0e1e46ef3ebfefa3df8ac27b12c
Abdelrahman Mohamed Ayman <abdelrhman.ayman753@gmail.com>
2023-09-02 23:17:36 +0300

    Added route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { AboutComponent } from './pages/about/about.component';
+import { AuthGuard, NegateAuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path:"home", loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
   {path:"store", loadChildren: () => import('./pages/store/store.module').then(m => m.StoreModule)},
-  {path:"cart", component: CartComponent},
+  {path:"cart", component: CartComponent,canActivate:[AuthGuard]},
   {path:"about", component: AboutComponent},
-  {path:"signin", component: SignInComponent},
-  {path:"signup", component: SignUpComponent},
+  {path:"signin", component: SignInComponent, canActivate:[NegateAuthGuard]},
+  {path:"signup", component: SignUpComponent, canActivate:[NegateAuthGuard]},
   {path: '', redirectTo:"home", pathMatch: 'full'},
   {path: "**", loadChildren: () => import('./pages/error/error.module').then(m => m.ErrorModule)},
 
